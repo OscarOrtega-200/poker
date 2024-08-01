f9053120d8fee3ae48d2a3189f4fbeecf7a1e6e3
OscarOrtega-200 <oscarfernandoortegarojas@gmail.com>
2024-07-31 23:19:21 -0500

    juego de poker v1

---
+++ poker.php
@@ -0,0 +1,27 @@
+<?php
+function repartirCartas(){
+    $mazo =[
+        'Corazones' => ["As","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","J","Q","K"],
+        'Picas' => ["As","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","J","Q","K"],
+        'Treboles' => ["As","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","J","Q","K"],
+        'Diamantes' => ["As","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","J","Q","K"],
+    ];
+    $cartas =[];
+    for ( $i = 0; $i < 5; $i++){
+        $palo = array_rand($mazo);
+        $valor =array_rand($mazo[$palo]);
+        $cartas[]=['palo' => $palo, 'valor' => $mazo[$palo][$valor]];
+    }
+    return $cartas; 
+}
+
+function mostrarCartas($cartas){
+    echo "Tus cartas son: \n";
+    foreach($cartas as $carta){
+        echo "{$carta ['valor']} de {$carta['palo']} \n";
+    } 
+}
+
+$cartas = repartirCartas();
+mostrarCartas($cartas);
+?>
